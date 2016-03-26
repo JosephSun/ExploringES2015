b29fdc4b7055a51b22ec1d06928942f7c05aa6f9
BKhurshid <josephsun4@gmail.com>
2016-03-25 19:42:52 -0700

    added es5

--- concepts/Destructuring.js
+++ concepts/Destructuring.js
@@ -1,3 +1,10 @@
-function initialize({controls = {}, models = {}, reducers = {}, actors = []}) {
-  
+function initializeES2015({controls = {}, models = {}, reducers = {}, actors = []}) {
+
+}
+
+function initializeES5 (options) {
+  var controls = options.controls || {};
+  var models = options.models || {};
+  var reducers = options.reducers || {};
+  var actors = options.actors || {};
 }
