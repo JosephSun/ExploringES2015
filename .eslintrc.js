c2615a2bcd36818b9c059f2e680e421c580bfbf3
BKhurshid <josephsun4@gmail.com>
2016-03-25 20:27:46 -0700

    added no dupe-args/case, noEmptyBlock, noExpectionAssging, noDoubleNegBoolean

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,11 @@ module.exports = {
     "rules": {
         "comma-dangle": "error",
         "no-cond-assign": "error",
+        "no-dupe-args": "error",
+        "no-dupe-case": "error",
+        "no-empty": "error",
+        "no-ex-assign": "error",
+        "no-extra-boolean-cast": "error",
         "semi": 2
     }
 
