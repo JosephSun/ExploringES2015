1bcef88e30e612d2a2f92e51b5c32c1dc2cf3a85
BKhurshid <josephsun4@gmail.com>
2016-03-26 19:07:24 -0700

    Creating example for Derived classes, subclassing

--- concepts/Classes.js
+++ concepts/Classes.js
@@ -20,3 +20,24 @@ class PersonES6 {
 }
 
 // let ali = new Person('ali');
+
+/*
+Derived classes:
+Subclassing is complicated in ES5, especially referring to
+super-constructors and super-properties. This is the
+canonical way of creating a sub-constructor of Person,
+Employee:
+*/
+
+function Employee(name, title) {
+  Person.call(this, name); // super(name)
+  this.title = title;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+Employee.prototype.describe = function (){
+  // super.describe()
+  return `${Person.prototype.describe.call(this)} (${this.title})`;
+
+}
